4fa2cf640492acdb19bbfa9116cb9a2ab4222e97
Pau Valverde <pvalverdemolina@gmail.com>
2024-10-21 20:02:34 +0900

    Initial Commit

---
+++ Bookworm/BookwormApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookwormApp.swift
+//  Bookworm
+//
+//  Created by Pau Valverde Molina on 10/21/24.
+//
+
+import SwiftUI
+
+@main
+struct BookwormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
